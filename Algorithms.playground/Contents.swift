57409d6d9a244a0b26999bafdd701a9fdfb0f5da
Abel Ferreira <abel_ferreira@apple.com>
2017-04-16 20:34:10 -0700

    first commit, setting up Playground project.

---
+++ Algorithms.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+//Fibonacci
+
+//(nth = 7) = 0, 1, 1, 2, 3, 5, 8
+
+
+func fibonacci(of: Int) -> Int {
+    return 1;
+}
+
+print(fibonacci(of: 1))
